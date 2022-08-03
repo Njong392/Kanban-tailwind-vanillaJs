9993f9a317fb853ca95f664e1e1fc39c8acd9ffd
Njong <njongemy@gmail.com>
2022-08-03 10:06:22 +0100

    Js for interactivity

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+/*
 //selectors
 const todoInput = document.querySelector('#todo-input');
 const todoButton = document.querySelector('#todo-button');
@@ -46,4 +47,79 @@ function addToDo(event) {
 
     //clearing the todo input value
     todoInput.value = '';
+}
+*/
+
+//selectors
+const input = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const cancel = document.querySelector('#cancel');
+const section1 = document.querySelector('#section1');
+const selector = document.querySelector('#selector');
+
+//event listeners
+submit.addEventListener('click', addToDo);
+
+//functions
+function addToDo(event) {
+    event.preventDefault();
+
+    if(input.value != ''){
+    const outerDiv = document.createElement('div');
+    outerDiv.classList.add('rounded-sm', 'shadow-lg', 'p-4', 'border-2', 'transform', 'hover:scale-105', 'mt-3');
+    section1.appendChild(outerDiv);
+
+    const flexDiv = document.createElement('div');
+    flexDiv.classList.add('flex');
+    outerDiv.appendChild(flexDiv);
+
+    const checkButton = document.createElement('div');
+    checkButton.classList.add('rounded-full', 'w-4', 'h-4', 'border-2', 'mr-4', 'mt-1', 'border-gray-500');
+    flexDiv.appendChild(checkButton);
+
+    const toDoDiv = document.createElement('div')
+    toDoDiv.classList.add('w-full');
+    flexDiv.appendChild(toDoDiv);
+
+    const toDo = document.createElement('p');
+    toDo.innerText = input.value;
+    toDoDiv.appendChild(toDo);
+
+    const choiceDiv = document.createElement('div');
+    choiceDiv.classList.add('flex', 'justify-between', 'mt-2');
+    toDoDiv.appendChild(choiceDiv);
+
+    const priorityP = document.createElement('p');
+    choiceDiv.appendChild(priorityP);
+
+    const priority = document.createElement('p');
+    if(selector.value = "High"){
+        priorityP.classList.add('border-2', 'border-red-600', 'rounded-sm', 'px-2', 'bg-red-200');
+        priority.classList.add('text-red-600');
+        
+    }
+    else if(selector.value = "Medium"){
+        priorityP.classList.add('border-2', 'border-green-600', 'rounded-sm', 'px-2', 'bg-green-200');
+        priority.classList.add('text-green-600');
+        
+    }
+    else{
+        priorityP.classList.add('border-2', 'border-yellow-600', 'rounded-sm', 'px-2', 'bg-yellow-200');
+        priority.classList.add('text-yellow-600');
+
+    }
+    priority.innerText = selector.value;
+    priorityP.appendChild(priority);
+
+   
+    
+
+    }
+    else{
+        console.log('enter a task');
+    }
+
+
+
+
 }
